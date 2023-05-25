1943ce9e7654d265ad9e8d7a0db0a3a4cbf0499a
SH <vhsqlqjs4679@naver.com>
2023-05-26 08:54:23 +0900

    Initial Commit

---
+++ Netflix_Clone_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix_Clone_App
+//
+//  Created by 초코크림 on 2023/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
